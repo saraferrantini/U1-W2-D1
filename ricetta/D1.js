1c6d08fbf1ace2a2d2412e6b8fefcb7995ceadf9
saraferrantini <sara.ferrantini@gmail.com>
2024-01-22 20:03:40 +0100

    modifiche

---
+++ ricetta/D1.js
@@ -0,0 +1,18 @@
+let ing1 = "spaghetti,";
+let ing2 = "uovo intero,";
+let ing3 = "guanciale,";
+let ing4 = "tuorlo d'uovo,";
+let ing5 = "pecorino romano DOP";
+let ing6 = "e pepe.";
+
+let ingredienti =
+  "Per fare la carbonara devi avere degli ingredienti che sono:" +
+  ing1 +
+  ing2 +
+  ing3 +
+  ing4 +
+  ing5 +
+  ing6 +
+  " " +
+  "Così potrai gustare una buona carbonara,secondo la tradizione Romana.";
+console.log(ingredienti);
